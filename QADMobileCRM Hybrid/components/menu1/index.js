fbd3abbf01a16c6fe1d86ddd9e333be24adef963
Raj Kumar <yrs@qad.com>
2015-10-15 16:59:08 +0000

    Changes from Screen Builder

---
+++ QADMobileCRM Hybrid/components/menu1/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.menu1 = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_menu1
+// END_CUSTOM_CODE_menu1
---
+++ QADMobileCRM Hybrid/components/search/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.search = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_search
+// END_CUSTOM_CODE_search
